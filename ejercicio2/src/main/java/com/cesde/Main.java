3e17132f3dc09d9107603b9f11fa6a88ce5f3a7e
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-14 12:13:48 -0500

    solucion2

--- ejercicio2/src/main/java/com/cesde/Main.java
+++ ejercicio2/src/main/java/com/cesde/Main.java
@@ -2,6 +2,17 @@ package com.cesde;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola Profe Jhon");
+
+        /* Número Par o Impar */
+
+        int numero = 8;
+
+        if (numero % 2 == 0) {
+            System.out.println("este numero es par");
+        } else {
+            System.out.println("este numero es impar");
+        }
+
     }
 }
