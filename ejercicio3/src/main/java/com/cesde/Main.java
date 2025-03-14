4f869da5ede15247a78f9cce4242da8d679cb1e8
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-14 12:40:24 -0500

    solucion3

--- ejercicio3/src/main/java/com/cesde/Main.java
+++ ejercicio3/src/main/java/com/cesde/Main.java
@@ -2,6 +2,19 @@ package com.cesde;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola Profe Jhon!");
+
+        /* Temperatura y Estado del Agua */
+
+        int temp = 110;
+
+        if (temp <= 0) {
+            System.out.println("El estado del agua es solido ");
+        } else if (temp > 0 && temp <= 100) {
+            System.out.println("El estado del agua es liquido");
+        } else {
+            System.out.println("El estado del agua es gaseoso");
+        }
+
     }
 }
