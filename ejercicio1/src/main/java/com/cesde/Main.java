c96cf0d893fd39e9eb1f47264d4e2214816772cc
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-14 12:02:17 -0500

    solucion1

--- ejercicio1/src/main/java/com/cesde/Main.java
+++ ejercicio1/src/main/java/com/cesde/Main.java
@@ -2,6 +2,20 @@ package com.cesde;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hola Profe Jhon");
+
+        /* Comparación de Números */
+
+        int a = 20;
+        int b = 10;
+
+        if (a > b) {
+            System.out.println("a es mayor que b");
+        } else if (a == b) {
+            System.out.println("a y b son iguales");
+        } else if (a != b) {
+            System.out.println("a es diferente que b");
+        }
+
     }
 }
