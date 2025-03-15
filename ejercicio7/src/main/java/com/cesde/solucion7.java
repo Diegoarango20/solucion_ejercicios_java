f17b940d50d01557426c9067300af0e751bf9c9d
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-14 23:41:19 -0500

    solucion7

---
+++ ejercicio7/src/main/java/com/cesde/solucion7.java
@@ -0,0 +1,28 @@
+package com.cesde;
+
+public class solucion7 {
+    public static void main(String[] args) {
+        System.out.println("Hola Profe Jhon!");
+
+        /* Calculadora de IMC */
+
+        double peso = 75;
+        double altura = 1.75;
+
+        double imc = peso / (altura * altura);
+
+        if (imc < 18.5) {
+            System.out.println("Bajo peso");
+
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("Normal");
+
+        } else if (imc >= 25 && imc <= 29.9) {
+            System.out.println("Sobrepeso");
+
+        } else if (imc >= 30) {
+             System.out.println("Obesidad");
+        }
+
+    }
+}
