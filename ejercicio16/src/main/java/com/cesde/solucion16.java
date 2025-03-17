3541d41541d6c9d2661836b1a80b13881d2b2998
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-17 00:02:03 -0500

    solucion16

--- ejercicio16/src/main/java/com/cesde/solucion16.java
+++ ejercicio16/src/main/java/com/cesde/solucion16.java
@@ -4,11 +4,32 @@ public class solucion16 {
     public static void main(String[] args) {
         System.out.println("Hola Profe Jhon!");
 
+        /* Operador Ternario */
+
+        // Expresión 1
+
         int edad = 20;
 
         String mensaje = ( edad >= 18) ?  "Eres mayor de edad" : "Eres menor de edad";
 
         System.out.println(mensaje);
+
+        // Expresión 2
+
+        int numero = -3;
+
+        String resultado = numero > 0 ? "Positivo" :  (numero < 0 ? "negativo" : "cero");
+
+        System.out.println(resultado);
+
+
+
+        
+
+
+
+
+
 
     }
 }
