96228964a333a7fd7264dc86be089de711787c96
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-17 11:43:32 -0500

    solucion14

--- ejercicio14/src/main/java/com/cesde/solucion14.java
+++ ejercicio14/src/main/java/com/cesde/solucion14.java
@@ -4,19 +4,15 @@ public class solucion14 {
     public static void main(String[] args) {
         System.out.println("Hola Profe Jhon!");
 
-        
+        // Verificador de Año Bisiesto
 
+        int año = 2020;
 
-
-
-
-
-
-
-
-
-
-
+        if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0)) {
+            System.out.println(año + " Es año bisiesto.");
+        } else {
+            System.out.println(año + " No es año bisiesto.");
+        }
 
     }
 }
