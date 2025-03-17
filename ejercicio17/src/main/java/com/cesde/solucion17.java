03f4d85a7dfef9ddba032c0d31c45a7a0acdff2e
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-17 00:16:17 -0500

    solucion17

---
+++ ejercicio17/src/main/java/com/cesde/solucion17.java
@@ -0,0 +1,34 @@
+package com.cesde;
+
+public class solucion17 {
+    public static void main(String[] args) {
+        System.out.println("Hola Profe Jhon!");
+
+        // Operaciones Combinadas
+
+        double numero1 = 50;
+        double numero2 = 150;
+        double numero3 = 300;
+
+        double suma = numero1 + numero2 + numero3;
+        double multiplicacion = numero1 * numero2 * numero3;
+        double promedio = (numero1 + numero2 + numero3) / 3;
+
+        System.out.println(suma);
+        System.out.println(multiplicacion);
+        System.out.println(promedio);
+    
+        if (promedio > suma) {
+            System.out.println("promedio es mayor que la suma");
+            
+        } else {System.out.println("promedio No es mayor que la suma");}
+
+
+
+
+
+
+
+
+    }
+}
