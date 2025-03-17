85247d28f40e0cbbb086d0556b6db4047201c596
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-17 11:58:35 -0500

    pendiente

--- ejercicio15/src/main/java/com/cesde/solucion15.java
+++ ejercicio15/src/main/java/com/cesde/solucion15.java
@@ -3,5 +3,23 @@ package com.cesde;
 public class solucion15 {
     public static void main(String[] args) {
         System.out.println("Hola Profe Jhon!");
+
+        // Calculadora de Descuento por Lealtad
+        // Este si se me dificulto profe
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
