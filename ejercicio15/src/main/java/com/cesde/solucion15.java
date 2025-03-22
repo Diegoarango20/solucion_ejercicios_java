325504fe139fd1c98e8346f87af1ad2ee7fa05f2
Diegoarango20 <diego_arango7@hotmail.com>
2025-03-21 21:08:58 -0500

    solucion15

--- ejercicio15/src/main/java/com/cesde/solucion15.java
+++ ejercicio15/src/main/java/com/cesde/solucion15.java
@@ -1,25 +1,53 @@
 package com.cesde;
 
+import java.util.Scanner;
+
 public class solucion15 {
     public static void main(String[] args) {
-        System.out.println("Hola Profe Jhon!");
+        Scanner scanner = new Scanner(System.in);
 
         // Calculadora de Descuento por Lealtad
-        // Este si se me dificulto profe
+
+        // Solicitar al usuario los años de cliente
+
+        System.out.print("Introduce los años de cliente: ");
+        int añosCliente = scanner.nextInt();
+
+
+        // Solicitar al usuario el monto de la compra
+
+        System.out.print("Introduce el monto de la compra: ");
+        double montoCompra = scanner.nextDouble();
 
         
+        double porcentajeDescuento = 0;
 
+        // Calcular el descuento 
 
+        if (añosCliente >= 1 && añosCliente <= 2) {
+            porcentajeDescuento = 5; 
+        } else if (añosCliente >= 3 && añosCliente <= 5) {
+            porcentajeDescuento = 10; 
+        } else if (añosCliente > 5) {
+            porcentajeDescuento = 15;
+        }
 
+        // compra supera $500
 
+        if (montoCompra > 500) {
+            porcentajeDescuento += 5; 
+        }
 
+        // Calcular el descuento total
 
+        double descuentoTotal = (porcentajeDescuento / 100) * montoCompra;
+        double montoFinal = montoCompra - descuentoTotal;
 
+        // Resultados
 
-
-
-
-
+        System.out.println("Descuento: " + porcentajeDescuento + "%");
+        System.out.println("Total después del descuento: $" + montoFinal);
 
     }
+
 }
